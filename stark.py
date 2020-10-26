d76cfaae68414cdd6d8fa832cc20a546b5928acf
EliashEfti <72147307+EliashEfti@users.noreply.github.com>
2020-10-26 11:29:26 +0600

    Update stark.py

--- stark.py
+++ stark.py
@@ -19,9 +19,66 @@ def menu():
          .##....##....##....##.....##.##....##..##...##.
          ..######.....##....##.....##.##.....##.##....##
 			       3.0
- >> CREATED BY:ANIKET STARK and Unknown_Girl404
- >> Youtube:   GamersTech330
- >> Instagram: @aniketstark330
+ >> CREATED BY:Eliash STARK and BCF-Bangladesh
+ >> Facebook :   Mohammad Eliash Efti
+ >> Instagram: @eliash
+ >>>ONLY FOR TERMUX<<<
+
+ ===============================================
+ 1. Basic Command
+ 2. Account Penetration
+ 3. Website Penetration
+ 4. Hash Cracker
+ 5. Termux
+ 6. Error Fixer
+ 7. Open Old STARK2.0""","green"), colored("""
+ I. Important Application's""","cyan", attrs=['bold']), colored("""
+ M. Hacking Movie's""","green"), colored("""
+ S. Secret			There Is No Secret0_0""","red", attrs=['bold']), colored("""
+ F. Follow Us""","magenta", attrs=['bold']), colored("""
+ C. Credits""","white", attrs=['bold']), colored("""
+ U. Update""","blue", attrs=['bold']), colored("""
+ ================================================""","green"), colored("""
+ H. Help""","yellow", attrs=['bold']), colored("""
+ E. EXIT
+ ================================================
+ """,'green')
+
+loop = True
+
+while loop:
+    menu()
+    stark = raw_input("stark > ")
+
+    if stark == "1":
+          os.system("clear")
+          BasicC()
+    elif stark == "2":
+          os.system("clear")
+          AccountH()# encoding: utf-8
+import os
+import sys
+from time import sleep as timeout
+from core.starkmcore import *
+from core.guidemcore import *
+from multiprocessing import Process
+from termcolor import colored
+
+checkfile()
+def menu():
+ os.system("clear")
+ print colored("""
+         ..######..########....###....########..##....##
+         .##....##....##......##.##...##.....##.##...##.
+         .##..........##.....##...##..##.....##.##..##..
+         ..######.....##....##.....##.########..#####...
+         .......##....##....#########.##...##...##..##..
+         .##....##....##....##.....##.##....##..##...##.
+         ..######.....##....##.....##.##.....##.##....##
+			       3.0
+ >> CREATED BY:Eliash STARK and BCF-Bangladesh
+ >> Facebook :   Mohammad Eliash Efti
+ >> Instagram: @eliash
  >>>ONLY FOR TERMUX<<<
 
  ===============================================
@@ -56,6 +113,19 @@ while loop:
     elif stark == "2":
           os.system("clear")
           AccountH()
+    elif stark == "3":
+          os.system("clear")
+          WebH()
+    elif stark == "4":
+          os.system("clear")
+          HASH()
+    elif stark == "5":
+          os.system("clear")
+          Termux()
+    elif stark == "6":
+          EFixer()
+    elif stark == "i" or stark == "I":
+		 checkofflineapp(
     elif stark == "3":
           os.system("clear")
           WebH()
